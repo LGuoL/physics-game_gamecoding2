e267c9731ff79c6531a5df74e232baa729db9784
LGuoL <2741629181@qq.com>
2026-04-01 03:20:22 +0800

    3.31

--- physicsGame_gamecoding2/Assets/interactableObject.cs
+++ physicsGame_gamecoding2/Assets/interactableObject.cs
@@ -2,15 +2,52 @@ using UnityEngine;
 
 public class InteractableObject : MonoBehaviour
 {
+    public Color highLightColor = new Color(1f, 0.95f, 0.6f);
+
+    [Range(0, 1f)] public float highLightStrangth = .4f;
+
+    private Renderer objectRenderer;
+
+    private Color originalColor;
+
+    private bool isHighLighted = false;
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        
+        objectRenderer = GetComponent<Renderer>();
+        if (objectRenderer != null)
+        {
+            originalColor = objectRenderer.sharedMaterial.color;
+        }
+
+        else
+        {
+            Debug.Log($"Interactable object on {gameObject.name} has no renderer. highLightwont work");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void HighLight()
+
     {
-        
+        if (isHighLighted || objectRenderer == null)
+        {
+            Debug.Log("no obj renderer & ishighlighted is true");
+            return;
+        }
+
+        objectRenderer.material.color = Color.Lerp(originalColor, highLightColor, highLightStrangth);
+        isHighLighted = true;
     }
+
+    public void UnhighLight()
+    {
+        if (!isHighLighted || objectRenderer == null) return;
+        objectRenderer.material.color = originalColor;
+        isHighLighted = false;
+    }
+
+
 }
